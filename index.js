3b8b1c13373045e9ccbafcd0aca1f6ad522be994
Senagoka <gokasena24@gmail.com>
2021-04-20 15:29:46 +0000

    added some changes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const connectDB = require("./config/connectDB");
 const traineesRoute = require("./routes/traineesRoute");
 const usersRoute = require("./routes/usersroute");
+const cors = require ("cors")
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ connectDB();
 // middlwares
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors());
 
 // route
 app.use("/api/v1/trainee", traineesRoute);
